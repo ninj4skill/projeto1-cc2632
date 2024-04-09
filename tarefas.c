63bd4d6aca672e2092ec55486a9a072255fe32f5
Onabe142 <160777855+Onabe142@users.noreply.github.com>
2024-04-09 20:33:20 +0000

    quebra de linha não armazenada

--- tarefas.c
+++ tarefas.c
@@ -8,15 +8,21 @@ ERROS criar(Tarefa tarefas[], int *pos){
 
     printf("Entre com a prioridade: ");
     scanf("%d", &tarefas[*pos].prioridade);
-    clearBuffer();
-    printf("Entre com a categoria: ");
-    fgets(tarefas[*pos].categoria, 100, stdin);
+    if(tarefas[*pos].prioridade <1 || tarefas[*pos].prioridade > 10)
+      printf("Prioridade inválida\n");
+    else{
+      
+      clearBuffer();
+      printf("Entre com a categoria: "); 
+      fgets(tarefas[*pos].categoria, 100, stdin); 
+      tarefas[*pos].categoria[strcspn(tarefas[*pos].categoria,"\n")] = '\0';
+      printf("Entre com a descricao: ");
+      fgets(tarefas[*pos].descricao, 300, stdin);
+      tarefas[*pos].categoria[strcspn(tarefas[*pos].categoria,"\n")] = '\0';
 
-    printf("Entre com a descricao: ");
-    fgets(tarefas[*pos].descricao, 300, stdin);
-
-    *pos = *pos + 1;
-
+      *pos = *pos + 1;
+ 
+      }
     return OK;
 }
 
